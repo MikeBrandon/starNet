9e4449b613098f393384d3faba049922c4d9c323
Mike Brandon <bkb.braock@gmail.com>
2021-10-15 14:09:11 +0300

    update: run js

--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,24 @@
 const main = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     const starContractFactory = await hre.ethers.getContractFactory('StarNet');
     const starContract = await starContractFactory.deploy();
     await starContract.deployed();
+
     console.log("Contract Deployed to: ", starContract.address);
+    console.log("Contract Deployed by: ", owner.address);
+
+    let starCount;
+    starCount = await starContract.getTotalStars();
+    
+    let starTxn = await starContract.sendStar();
+    await starTxn.wait();
+
+    starCount = await starContract.getTotalStars();
+
+    starTxn = await starContract.connect(randomPerson).sendStar();
+    await starTxn.wait();
+
+    starCount = await starContract.getTotalStars();
 }
 
 const runMain = async () => {
