f59d53c6f8b8517e98691dd084ab59a546290cef
Mike Brandon <bkb.braock@gmail.com>
2021-10-15 14:19:14 +0300

    add: rinkeby url  and pk

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,7 +7,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
-    console.log(account.address);
+    console.log(account.address); 
   }
 });
 
@@ -19,4 +19,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.0",
+  networks: {
+    rinkeby: {
+      url: process.env.RINKEBY_URL,
+      accounts: [process.env.RINKEBY_PK]
+    }
+  }
 };
