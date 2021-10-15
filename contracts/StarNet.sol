087ddd66364013532b6404343ebf8dd6c37cc96d
Mike Brandon <bkb.braock@gmail.com>
2021-10-15 12:59:11 +0300

    add: sendStar and getTotalStars functions

--- contracts/StarNet.sol
+++ contracts/StarNet.sol
@@ -5,7 +5,19 @@ pragma solidity ^0.8.0;
 import 'hardhat/console.sol';
 
 contract StarNet {
+    uint256 totalStars;
+
     constructor() {
         console.log('Welcome to StarNet Contract');
+    }
+
+    function sendStar() public {
+        totalStars += 1;
+        console.log('%s just sent a Star!', msg.sender);
+    }
+
+    function getTotalStars() public view returns (uint256) {
+        console.log('We have %d stars', totalStars);
+        return totalStars;
     }
 }
