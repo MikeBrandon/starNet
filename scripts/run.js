d8ca659fc0d4ed024d6104a4032048da9a5b252b
Mike Brandon <bkb.braock@gmail.com>
2021-10-16 12:49:19 +0300

    update: run.js test

--- scripts/run.js
+++ scripts/run.js
@@ -1,11 +1,21 @@
 const main = async () => {
     const [owner, randomPerson] = await hre.ethers.getSigners();
     const starContractFactory = await hre.ethers.getContractFactory('StarNet');
-    const starContract = await starContractFactory.deploy();
+    const starContract = await starContractFactory.deploy({
+        value: hre.ethers.utils.parseEther('0.1')
+    });
     await starContract.deployed();
 
     console.log("Contract Deployed to: ", starContract.address);
     console.log("Contract Deployed by: ", owner.address);
+
+    let contractBalance = await hre.ethers.provider.getBalance(
+        starContract.address
+    );
+    console.log(
+        'Contract balance:',
+        hre.ethers.utils.formatEther(contractBalance)
+    );
 
     let starCount;
     starCount = await starContract.getTotalStars();
@@ -21,6 +31,12 @@ const main = async () => {
     starCount = await starContract.getTotalStars();
     let allStars = await starContract.getAllStars();
     console.log(allStars);
+
+    contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+    console.log(
+        'Contract balance:',
+        hre.ethers.utils.formatEther(contractBalance)
+    );
 }
 
 const runMain = async () => {
