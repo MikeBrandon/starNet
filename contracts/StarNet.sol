ffe1d9bcd0bdd907201b55b8b2322ec6201456c8
Mike Brandon <bkb.braock@gmail.com>
2021-10-16 14:17:44 +0300

    add: cooldown timer of 15min

--- contracts/StarNet.sol
+++ contracts/StarNet.sol
@@ -6,7 +6,6 @@ import 'hardhat/console.sol';
 
 contract StarNet {
     uint256 totalStars;
-
     uint256 private seed;
 
     event NewStar(address indexed from, uint256 timestamp, string message);
@@ -20,12 +19,17 @@ contract StarNet {
     }
 
     Star[] stars;
+    mapping(address => uint256) public lastWavedAt;
 
     constructor() payable {
         console.log('Welcome to StarNet Contract');
     }
 
     function sendStar(string memory _message) public {
+
+        require((lastWavedAt[msg.sender] + 15 minutes) < block.timestamp, 'Wait 15min');
+        lastWavedAt[msg.sender] = block.timestamp;
+
         totalStars += 1;
         console.log('%s just sent a Star!', msg.sender);
 
